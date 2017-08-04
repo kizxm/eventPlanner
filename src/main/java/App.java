30cb374ee5082471cb4620b2ffcf8eaf95724a7f
Kira Loo <youhavebeenkiraed@gmail.com>
2017-08-04 16:53:35 -0700

    currently app doesn't crash, however, coupon codes do NOT work & tests are incomplete.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,17 +4,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-
 public class App {
     public static void main(String[] args) {
+
         ArrayList<Event> partyDetails = new ArrayList<>();
 
         boolean programRunning = true;
         while (programRunning) {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("Welcome to Kira's Party Planning! Let's plan your party! >>> 'Plan Party', 'Party Cost', or 'Exit'.");
+            System.out.println("Welcome to Kira's Party Planning! Let's plan your party! >>> 'Plan Party', 'Party Cost', 'Use a Coupon', or 'Exit'.");
 
             try {
+
                 String navigationChoice = bufferedReader.readLine();
                 if (navigationChoice.equals("Plan Party")) {
                     System.out.println("How many guests?");
@@ -28,8 +29,6 @@ public class App {
                     Event newEvent = new Event(inputGuests, inputDrink, inputLocation, inputEntertainment);
                     partyDetails.add(newEvent);
                 }
-
-
 
 //                currently, non-functional; was trying to create a separate selection for entertainment selection that would convert
 //                a string into an integer that could then be added to my partyCost method.
@@ -47,7 +46,6 @@ public class App {
 //                    }
 //                }
 
-
                 else if (navigationChoice.equals("Party Cost")){
                     for (Event oneEvent : partyDetails) {
                         int eventCost = oneEvent.partyCost();
@@ -56,10 +54,29 @@ public class App {
                     }
                 }
 
+                else if (navigationChoice.equals("Use a Coupon")) {
+                    for (Event discountEvent1 : partyDetails) {
+                        int eventCost1 = discountEvent1.partyCouponCost1();
+
+                        for (Event discountEvent2 : partyDetails) {
+                            int eventCost2 = discountEvent2.partyCouponCost2();
+
+                            System.out.println("Apply a coupon?; '1FREEDJ', '$20OFF'");
+                            if (bufferedReader.readLine().equals("1FREEDJ")) {
+                                System.out.println(String.format("New total with Coupon: ", eventCost1));
+                            } else if (bufferedReader.readLine().equals("$20OFF")) ;
+                            {
+                                System.out.println(String.format("New total with Coupon:", eventCost2));
+                            }
+                        }
+                    }
+                }
+
                 else if (navigationChoice.equals("Exit")) {
                     System.out.println("See you next time!");
                     programRunning = false;
                 }
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -13,7 +13,16 @@ public class Event {
         this.partyEntertainment = partyEntertainment;
 
     }
+
     public int partyCost() {
         return ((this.partyDrinks * 20) + (this.partyGuests * 2) + (this.partyLocation * 50) + (this.partyEntertainment * 250) + 10);
     }
+
+    public int partyCouponCost1() {
+        return ((partyCost() - 500));
+    }
+    public int partyCouponCost2() {
+        return ((partyCost() - 20));
+    }
 }
+
