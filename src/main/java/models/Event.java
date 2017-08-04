0966a965ed2b2239969773ccb3bbe29492054b0a
Kira Loo <youhavebeenkiraed@gmail.com>
2017-08-04 10:05:44 -0700

    Fixed first test to not fail when adding properties.

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -1,7 +1,16 @@
 package models;
 
 public class Event {
+    public int partyGuests;
+    public int partyDrinks;
+
+    public Event(int partyGuests, int partyDrinks) {
+        this.partyGuests = partyGuests;
+        this.partyDrinks = partyDrinks;
+    }
 
 
-
+    public int partyCost() {
+        return ((this.partyDrinks) + (this.partyGuests) + 10);
+    }
 }
--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -1,5 +1,13 @@
+package models;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class EventTest {
+    @Test
+    public void eventAddsCorrectly () throws Exception {
+        Event testEvent = new Event(2, 2);
+        assertEquals(14, testEvent.partyCost());
+    }
+
 
 }
